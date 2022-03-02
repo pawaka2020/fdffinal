a03400b113025fabbbf39380731b8926ff64ffd7
Rinor Ramli <rinorramlifreelance@gmail.com>
2022-03-02 17:36:39 +0800

    second retry

---
+++ mylibft2/libft/ft_strchr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riramli <riramli@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/01 00:00:00 by riramli           #+#    #+#             */
+/*   Updated: 2022/10/01 00:00:00 by riramli          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/*
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return ((char *) s + i);
+		i++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
+#include <string.h>
+void test(int c)
+{
+	const char str[] = "abcde123";
+	printf("strchr: String starting from %c is: %s\n", c, strchr(str, c));
+	printf("strchr: String starting from %c is: %s\n\n", c, ft_strchr(str, c));
+}
+int main () {
+	int i = 32;
+	while (i < 127)
+		test(i++);
+}
+*/
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+		if (!*s++)
+			return (0);
+	return ((char *)s);
+}
